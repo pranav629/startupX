17b9da02f74595f75ab5346f5dd162c2f4e0ab9a
pranav629 <148237613+pranav629@users.noreply.github.com>
2025-04-07 19:51:26 +0530

    Delete StartupX directory

--- StartupX/folder2/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LuffyluffyConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'luffyluffy'
--- StartupX/search.py
+++
@@ -1,5 +0,0 @@
-from firebase_admin import firestore
-db=firestore.client()
-entrepreneurs=db.collection('Entrepreneurs').stream()
-ens=[doc.to_dict() for doc in entrepreneurs]
-print(ens)
